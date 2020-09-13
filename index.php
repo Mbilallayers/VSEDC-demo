2498cabbbf12b3fbf9781f9771c6442d74b8cc36
m-bilal85 <m.bilal@brandjaws.com>
2020-09-13 11:49:43 +0500

    thank you

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if (isset($_POST['submit']))
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Summer Camps Thank you</title>
+    <title>Summer Camps</title>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="assets/css/aos.css" />
     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css" />
